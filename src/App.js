b545207e03ef7b541dcf22d73ecc93bc4545fd67
Gustavo <60441031+Gustavox98@users.noreply.github.com>
2022-01-20 23:27:34 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, NavLink as Link, BrowserRouter } from 'react-router-dom';
+import Aboutus from './components/Aboutus.js';
+import Home from './components/home.js';
+import Projects from './components/projects.js';
 
-function App() {
+
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   
+    <div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">home</Link>
+        </li>
+        <li>
+        <Link to="Aboutus">Aboutus</Link>
+        </li>
+        <li>
+        <Link to="contact">contact</Link>
+        </li>
+        <li>
+        <Link to="projects">projects</Link>
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="Aboutus" element={<Aboutus />} />
+      <Route path="contact" element={<contact />} />
+      <Route path="projects" element={<projects />} />
+    </Routes>
+
+  </div>
+)
 }
 
-export default App;
